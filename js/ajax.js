8e98806f1c0e074a9754fc278b6a7c3c622fabca
Harman Pannu <iharmanpannu@gmail.com>
2018-03-03 12:39:06 -0800

    complete project

--- js/ajax.js
+++ js/ajax.js
@@ -1,39 +1,37 @@
 // fetch AJAX request
 
-(() => {
-  const url = "http://api.openweathermap.org/data/2.5/weather?q=Seattle,USA";
-  const apiKey = "05e62f86576a85bc20cce0e46674216f"; // Api key
-  let httpRequest;
+const url = "http://api.openweathermap.org/data/2.5/weather?q=Seattle,USA";
+const apiKey = "05e62f86576a85bc20cce0e46674216f"; // Api key
+let httpRequest;
 
-  fetch(`${url}&appid=${apiKey}`)
-    .then(function(response) {
-      if (!response.ok) {
-        throw Error(response.text);
-      }
-      return response.json();
-    })
-    .then(function(response) {
-      updateUISuccess(response);
-    })
-    .catch(function(error) {
-      updateUIError();
-    });
+fetch(`${url}&appid=${apiKey}`)
+  .then(function(response) {
+    if (!response.ok) {
+      throw Error(response.text);
+    }
+    return response.json();
+  })
+  .then(function(response) {
+    updateUISuccess(response);
+  })
+  .catch(function(error) {
+    updateUIError();
+  });
 
-  // Handle XHR success
-  function updateUISuccess(response) {
-    const condition = response.weather[0].main;
-    const degC = response.main.temp - 273.15;
-    const degCInt = Math.floor(degC);
-    const degF = degC * 1.8 + 32;
-    const degFInt = Math.floor(degF);
-    const weatherBox = document.getElementById("weather");
-    weatherBox.innerHTML = `<p>${degCInt}&#176; C / ${degFInt}&#176; F</p> <p>${condition}</p>`;
-  }
+// Handle XHR success
+function updateUISuccess(response) {
+  const condition = response.weather[0].main;
+  const degC = response.main.temp - 273.15;
+  const degCInt = Math.floor(degC);
+  const degF = degC * 1.8 + 32;
+  const degFInt = Math.floor(degF);
+  const weatherBox = document.getElementById("weather");
+  weatherBox.innerHTML = `<p>${degCInt}&#176; C / ${degFInt}&#176; F</p> <p>${condition}</p>`;
+}
 
-  // handle XHR error
+// handle XHR error
 
-  function updateUIError() {
-    const weatherBox = document.getElementById("weather");
-    weatherBox.className = "hidden";
-  }
-})();
+function updateUIError() {
+  const weatherBox = document.getElementById("weather");
+  weatherBox.className = "hidden";
+}
